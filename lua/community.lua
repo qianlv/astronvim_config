4c94fe86d822595b3a3e6167534f89d0fcf51634
qianlv <qianlv7@qq.com>
2025-03-18 17:24:43 +0800

    更新ai配置

--- lua/community.lua
+++ lua/community.lua
@@ -13,20 +13,16 @@ return {
   { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.completion.cmp-calc" },
   { import = "astrocommunity.completion.cmp-cmdline" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
-  -- { import = "astrocommunity.editing-support.vim-doge" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.editing-support.suda-vim" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.lsp.actions-preview-nvim" },
-  -- { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.lsp.nvim-lsp-endhints" },
-  -- { import = "astrocommunity.programming-language-support.rest-nvim" },
-  -- { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.split-and-window.colorful-winsep-nvim" },
   { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
 
@@ -45,15 +41,15 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.full-dadbod" },
 
-  {
-    "zbirenbaum/copilot.lua",
-    opts = function(_, opts)
-      opts.filetypes = {
-        markdown = true,
-      }
-      return opts
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   opts = function(_, opts)
+  --     opts.filetypes = {
+  --       markdown = true,
+  --     }
+  --     return opts
+  --   end,
+  -- },
 
   {
     "m4xshen/smartcolumn.nvim",
@@ -61,14 +57,4 @@ return {
       colorcolumn = { "80", "100", "120" },
     },
   },
-
-  -- {
-  --   "rest-nvim/rest.nvim",
-  --   config = function()
-  --     vim.api.nvim_create_autocmd("FileType", {
-  --       pattern = "json",
-  --       callback = function(ev) vim.bo[ev.buf].formatprg = "jq" end,
-  --     })
-  --   end,
-  -- },
 }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,10 @@
 -- cspell: disable
+--
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -17,20 +23,49 @@ return {
         }
       end,
     },
+    {
+      "milanglacier/minuet-ai.nvim",
+    }
   },
 
   opts = function(_, opts)
     local cmp = require "cmp"
+    local minuet = require('minuet.virtualtext')
+    local snip_status_ok, luasnip = pcall(require, "luasnip")
+    if not snip_status_ok then return end
+
     local sources = {
       {
         name = "dictionary",
         keyword_length = 2,
       },
+      -- { name = 'minuet' },
+    }
+
+    opts.performance = {
+      fetching_timeout = 2000,
     }
 
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, sources))
+
+    if not opts.mapping then opts.mapping = {} end
+        opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+          if minuet.action.is_visible() then
+            vim.defer_fn(minuet.action.accept, 30)
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" })
+
     opts.mapping["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" })
     opts.mapping["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" })
+    -- opts.mapping["<A-y>"] = require('minuet').make_cmp_map()
 
     return opts
   end,
