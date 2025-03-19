418fd3f1aeec9f409c9854e6c0e7579c9af97cf6
qianlv <qianlv7@qq.com>
2025-03-19 10:32:48 +0800

    添加ai配置

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    lazy = false,
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+        },
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+}
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,12 +25,12 @@ return {
     },
     {
       "milanglacier/minuet-ai.nvim",
-    }
+    },
   },
 
   opts = function(_, opts)
     local cmp = require "cmp"
-    local minuet = require('minuet.virtualtext')
+    local minuet = require "minuet.virtualtext"
     local snip_status_ok, luasnip = pcall(require, "luasnip")
     if not snip_status_ok then return end
 
@@ -49,19 +49,19 @@ return {
     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, sources))
 
     if not opts.mapping then opts.mapping = {} end
-        opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-          if minuet.action.is_visible() then
-            vim.defer_fn(minuet.action.accept, 30)
-          elseif cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" })
+    opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      if minuet.action.is_visible() then
+        vim.defer_fn(minuet.action.accept, 30)
+      elseif cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
 
     opts.mapping["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" })
     opts.mapping["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" })
